90568cc62850e2cdff0072bed9d9802d76625d89
Guilherme B <dogui0202@gmail.com>
2024-06-20 18:13:52 -0300

    Carrinho funcionando, medo de como vai ser levar isso pra responsividade pelo que eu fiz com o display flex para os itens ficarem de acordo

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 $(document).ready (function() {
-    let Carrinho = []
+    let Carrinho = [];
+    let Linhas = "";
 
-    $('#produtos button').click( function() {
-        let Item
-    })
+        $('#produtos button').click( function() {
 
+            AdicionarItem($(this));
+            AtualizarTabela();
+
+        })
+
+        function AdicionarItem(button) {
+
+            const ItemEscolhido = button.parent().find('h3').text();
+            const PrecoEscolhido = button.parent().find('strong').text();
+            const FotoEscolhida = button.parent().find('img').attr('src');
     
+            let Item = '<div class"item-container" style="display: flex;">';
+            Item += `<img src="${FotoEscolhida}">`
+            Item += '<div class="cart-item">'
+            Item += `<strong>${ItemEscolhido}</strong>`
+            Item += `<h3>R$ ${PrecoEscolhido}</h3>`
+            Item += '</div>'
+            Item += '</div>'
 
+            Linhas += Item
+        }
 
+    function AtualizarTabela () {
+        const corpoCarrinho = document.querySelector(".tabela-itens")
+        corpoCarrinho.innerHTML = Linhas;
+    }
 
-
+    function AtualizarTotal () {
+        
+    }
 })
