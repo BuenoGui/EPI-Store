85a6bed6a5babae4258563857b18e874db5d0159
Guilherme B <dogui0202@gmail.com>
2024-06-21 07:56:18 -0300

    Carrinho funcionando, falta adicionar quantidade de item

--- index.js
+++ index.js
@@ -1,37 +1,50 @@
 $(document).ready (function() {
     let Carrinho = [];
     let Linhas = "";
+    let PrecoTotal = 0
 
         $('#produtos button').click( function() {
 
-            AdicionarItem($(this));
+            AtualizarTotal($(this));
             AtualizarTabela();
-
         })
-
-        function AdicionarItem(button) {
-
-            const ItemEscolhido = button.parent().find('h3').text();
-            const PrecoEscolhido = button.parent().find('strong').text();
-            const FotoEscolhida = button.parent().find('img').attr('src');
-    
-            let Item = '<div class"item-container" style="display: flex;">';
-            Item += `<img src="${FotoEscolhida}">`
-            Item += '<div class="cart-item">'
-            Item += `<strong>${ItemEscolhido}</strong>`
-            Item += `<h3>R$ ${PrecoEscolhido}</h3>`
-            Item += '</div>'
-            Item += '</div>'
-
-            Linhas += Item
-        }
 
     function AtualizarTabela () {
         const corpoCarrinho = document.querySelector(".tabela-itens")
         corpoCarrinho.innerHTML = Linhas;
     }
 
-    function AtualizarTotal () {
-        
+    function AtualizarTotal (button) {
+
+    const ItemEscolhido = button.parent().find('h3').text();
+    const PrecoEscolhido = button.parent().find('#valor').text().replace(",",".");
+    const FotoEscolhida = button.parent().find('img').attr('src');
+
+        let Item = '<div class= "item-container">';
+        Item += `<img src="${FotoEscolhida}">`
+        Item += '<div class="cart-item">'
+        Item += `<strong>${ItemEscolhido}</strong>`
+        Item += `<h3>R$ ${PrecoEscolhido}</h3>`
+        Item += '</div>'
+        Item += '</div>'
+
+        Linhas += Item
+        Carrinho += ItemEscolhido
+
+        if(Carrinho.includes(ItemEscolhido.value)) {
+            
+        }
+    
+    let PrecoNum = parseFloat(PrecoEscolhido);
+    PrecoTotal += PrecoNum
+
+    const Total = document.getElementById("total")
+    Total.innerHTML = `Total: R$ ${PrecoTotal.toFixed(2).replace(".",",")}`
+    
+
+    console.log(Carrinho)
+
     }
+
+
 })
